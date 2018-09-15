f0c6e77fb8e8b4ddaba5ce14d1e0bdc7f361cdfa
Ido Lev <idolev@Ido-Lev.local>
2018-09-15 15:58:57 +0300

    add tests exapmles and update readme

--- utils.js
+++ utils.js
@@ -1,12 +1,24 @@
 // examples functions to be tested
-
-
-// first, install mocha: 'npm i mocha -D' or 'npm install moach --save-dev'
-
-// to run with nodemon - just type: nodemon --exec 'npm test'
  
 // add two numbers
 module.exports.add = (a, b) => a + b;
 
 // square number
 module.exports.square = number => number * number + 1;
+
+// set name for user object
+module.exports.setName = (user, fullname) => {
+    const splitted = fullname.split(' ');
+    user.firstName = splitted[0];
+    user.lastName = splitted[1];
+    return user;
+};
+
+// async function
+module.exports.asyncFunc = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('I am done');
+        }, 1000);
+    });
+};
